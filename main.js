06957ca6ad17f22132d53f28cdd5a0042fef0e95
umidjan143 <xasanbayevumidjon@gmail.com>
2023-07-20 21:02:28 +0500

    lesson5 homework

---
+++ main.js
@@ -0,0 +1,229 @@
+//tag name
+
+// const h1 = document.getElementsByTagName('h1');
+
+// h1.textContent = "111" ;
+
+// const h2 = document.getElementsByTagName('h2');
+
+// h2.textContent = "222" ;
+
+// const h3 = document.getElementsByTagName('h3');
+
+// h3.textContent = "33" ;
+
+// const h4 = document.getElementsByTagName('h4');
+
+// h4.textContent = "444" ;
+
+// const h5 = document.getElementsByTagName('h5');
+
+// h5.textContent = "55" ;
+
+// const h6 = document.getElementsByTagName('h6');
+
+// h6.textContent = "666" ;
+
+// const p = document.getElementsByTagName('p');
+
+// p.textContent = "it is text " ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//id
+
+// let h1 = document.getElementById('h1');
+
+// h1.textContent = " has got id from h1 " ;
+
+// let h2 = document.getElementById('h2');
+
+// h2.textContent = " has got id from h2 " ;
+
+// let h3 = document.getElementById('h3');
+
+// h3.textContent = " has got id from h3 " ;
+
+// let h4 = document.getElementById('h4');
+
+// h4.textContent = " has got id from h4 " ;
+
+// let h5 = document.getElementById('h5');
+
+// h5.textContent = " has got id from h5 " ;
+
+// let h6 = document.getElementById('h6');
+
+// h6.textContent = " has got id from h6 " ;
+
+// let p = document.getElementById('lorem-text');
+
+// p.textContent = " it is lorem text " ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//by class
+
+// let li1 = document.getElementsByClassName('element1');
+
+// li1.textContent = "first li of ul"
+
+// let li2 = document.getElementsByClassName('element2');
+
+// li2.textContent = "second li of ul"
+
+// let li3 = document.getElementsByClassName('element3');
+
+// li3.textContent = "third li of ul"
+
+// let li4 = document.getElementsByClassName('element4');
+
+// li4.textContent = "fourth li of ul"
+
+// let li5 = document.getElementsByClassName('element5');
+
+// li5.textContent = "fifth li of ul"
+
+// let li6 = document.getElementsByClassName('element6');
+
+// li6.textContent = "last li of ul"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// querySelector
+
+// let li1 = document.querySelector('#for-querySelector');
+
+// li1.textContent = "selected with query by id"
+
+// let li2 = document.querySelector('.for-querySelector');
+
+// li2.textContent = "selected with query by class"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // querySelectorAll
+
+// let li = document.querySelectorAll(".example");
+
+// li.textContent = "changed";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let count = 1000;
+
+// function upd() {
+//     value.textContent = count--;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function changeColor() {
+//     document.body.style.backgroundColor = 'yellow'
+// }
